8f28e3d3d85e42d36ab4dbe4c681421469571ad6
Rahela <rahelabobeanu@gmail.com>
2024-06-23 15:27:43 +0300

    first commit

---
+++ assets/components/UserList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>User List</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }} ({{ user.email }})
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users: []
+        };
+    },
+    created() {
+        this.fetchUsers();
+    },
+    methods: {
+        async fetchUsers() {
+            try {
+                const response = await axios.get('/api/users');
+                this.users = response.data;
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        }
+    }
+};
+</script>
---
+++ src/Entity/User.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource(
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'userEmail' => 'partial',
+    'firstName' => 'partial',
+    'lastName' => 'partial'
+])]
+#[ApiFilter(OrderFilter::class, properties: [
+    'id', 'userEmail', 'firstName', 'lastName', 'createdAt'
+])]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['user:read', 'user:write'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $userEmail = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $lastName = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['user:read'])]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getUserEmail(): ?string
+    {
+        return $this->userEmail;
+    }
+
+    public function setUserEmail(string $userEmail): static
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
