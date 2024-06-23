aa89ed149422bc8a8bd11f5429cbf24c29bbaebd
Rahela <rahelabobeanu@gmail.com>
2024-06-23 17:33:03 +0300

    add filtering

--- assets/components/UserList.vue
+++ assets/components/UserList.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
       <h1>User List</h1>
+      
+      <div>
+        <label for="sortColumn">Sort by:</label>
+        <select id="sortColumn" v-model="sortColumn">
+          <option value="firstName">First Name</option>
+          <option value="lastName">Last Name</option>
+          <option value="userEmail">Email</option>
+          <option value="id">ID</option>
+          <option value="createdAt">Created At</option>
+        </select>
+        <select v-model="sortOrder">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+        <button @click="fetchUsers">Sort</button>
+      </div>
+      
       <ul>
         <li v-for="user in users" :key="user.id">
           {{ user.firstName }} {{ user.lastName }} ({{ user.userEmail }})
@@ -38,7 +55,9 @@
           lastName: '',
           userEmail: ''
         },
-        selectedUserId: null
+        selectedUserId: null,
+        sortColumn: 'firstName',
+        sortOrder: 'asc'
       };
     },
     created() {
@@ -47,7 +66,13 @@
     methods: {
       async fetchUsers() {
         try {
-          const response = await axios.get('/api/users');
+          const response = await axios.get('http://127.0.0.1:8001/api/users', {
+            params: {
+              order: {
+                [this.sortColumn]: this.sortOrder
+              }
+            }
+          });
           this.users = response.data['hydra:member'];
         } catch (error) {
           console.error('Error fetching users:', error);
@@ -55,13 +80,14 @@
       },
       async addUser() {
         let axiosConfig = {
-        headers: {
-            'Content-Type':'application/ld+json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*"
-        }};
+          headers: {
+            'Content-Type': 'application/ld+json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*'
+          }
+        };
         try {
           const response = await axios.post(
-            'http://127.0.0.1:8001/api/users', 
+            'http://127.0.0.1:8001/api/users',
             this.formData,
             axiosConfig
           );
@@ -79,12 +105,17 @@
       },
       async updateUser() {
         let axiosConfig = {
-        headers: {
-            'Content-Type':'application/ld+json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*"
-        }};
+          headers: {
+            'Content-Type': 'application/ld+json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*'
+          }
+        };
         try {
-          await axios.put(`http://127.0.0.1:8001/api/users/${this.selectedUserId}`, this.formData, axiosConfig);
+          await axios.put(
+            `http://127.0.0.1:8001/api/users/${this.selectedUserId}`,
+            this.formData,
+            axiosConfig
+          );
           this.closeModal();
           this.fetchUsers();
         } catch (error) {
@@ -93,12 +124,16 @@
       },
       async deleteUser(user) {
         let axiosConfig = {
-        headers: {
-            'Content-Type':'application/ld+json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*"
-        }};
+          headers: {
+            'Content-Type': 'application/ld+json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*'
+          }
+        };
         try {
-          await axios.delete(`http://127.0.0.1:8001/api/users/${user.id}`, axiosConfig);
+          await axios.delete(
+            `http://127.0.0.1:8001/api/users/${user.id}`,
+            axiosConfig
+          );
           this.fetchUsers();
         } catch (error) {
           console.error('Error deleting user:', error);
